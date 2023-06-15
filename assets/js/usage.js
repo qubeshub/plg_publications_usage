afab9ec913076a9cc667fbb3c8f968f394bb6a75
M. Drew LaMar <mdlama@wm.edu>
2023-06-15 14:57:58 -0400

    [fix] mini-dash not working in sg pub views
    (cherry picked from commit 93f2fa6dd193fbd46f274b14d0c6e064e24b74b8)

--- assets/js/usage.js
+++ assets/js/usage.js
@@ -31,145 +31,148 @@ $(document).ready(function () {
     
     const url = $('#usage-form').attr('action') + '/' + $('#usage-form').attr('version')
 
-    $.ajax({
-        method: 'GET',
-        url: url.nohtml(),
-        dataType: 'json',
-        success: function (response, status, xhr) {
+    //Bind resize event to window object and invoke resize immediately on load
+    $(window).on('resize', function () {
+        $.ajax({
+            method: 'GET',
+            url: url.nohtml(),
+            dataType: 'json',
+            success: function (response, status, xhr) {
 
-            var views = $('#chart-views');
-            views.bind("plotclick", function(event, pos, item) {
-                return updateCharts(event, pos, item);
-            });
+                var views = $('#chart-views');
+                views.bind("plotclick", function (event, pos, item) {
+                    return updateCharts(event, pos, item);
+                });
 
-            let views_array = response.views.splice(0, response.views.length)
-            let views_split = []
-            let dataset_views = [{
-                color: response.dataset_views.color,
-                data: views_split,
-                label: response.dataset_views.label
-            }]
+                let views_array = response.views.splice(0, response.views.length)
+                let views_split = []
+                let dataset_views = [{
+                    color: response.dataset_views.color,
+                    data: views_split,
+                    label: response.dataset_views.label
+                }]
 
-            for (i = 0; i < views_array.length; i++) {
-                views_split.push(eval(views_array[i]))
-            }
-
-            var chart_views = $.plot(views, dataset_views, {
-                series: {
-                    lines: {
-                        show: true,
-                        fill: false
-                    },
-                    points: { show: false },
-                    shadowSize: 0
-                },
-                grid: {
-                    color: 'rgba(0, 0, 0, 0.6)',
-                    borderWidth: 1,
-                    borderColor: 'transparent',
-                    hoverable: true,
-                    clickable: true
-                },
-                tooltip: true,
-                    tooltipOpts: {
-                    content: "%y %s in %x",
-                    shifts: {
-                        x: -60,
-                        y: 25
-                    },
-                    defaultTheme: false
-                },
-                legend: {
-                    show: false
-                },
-                xaxis: {
-                    mode: "time",
-                    tickDecimals: 0,
-                    tickFormatter: function (val, axis) {
-                        var d = new Date(val);
-                        return (d.getUTCMonth() + 1) + "/" + d.getUTCFullYear().toString().substr(2,2);
-                    }
-                },
-                yaxis: {
-                    min: 0,
-                    tickFormatter: function (val, axis) {
-                        if (val > 1000) {
-                            val = (val / 1000) + ' K';
-                        }
-                        return val;
-                    }
+                for (i = 0; i < views_array.length; i++) {
+                    views_split.push(eval(views_array[i]))
                 }
-            });
-            chart_views.highlight(0, response.chart_views - 1);
 
-
-            var downloads = $('#chart-downloads');
-            downloads.bind("plotclick", function(event, pos, item) {
-                return updateCharts(event, pos, item);
-            });
-
-            let downloads_array = response.downloads.splice(0, response.downloads.length)
-            let downloads_split = []
-            let dataset_downloads = [{
-                color: response.dataset_downloads.color,
-                data: downloads_split,
-                label: response.dataset_downloads.label
-            }]
-            
-            for (i = 0; i < downloads_array.length; i++) {
-                downloads_split.push(eval(downloads_array[i]))
-            }
-
-            var chart_downloads = $.plot(downloads, dataset_downloads, {
-                series: {
-                    lines: {
-                        show: true,
-                        fill: false
+                var chart_views = $.plot(views, dataset_views, {
+                    series: {
+                        lines: {
+                            show: true,
+                            fill: false
+                        },
+                        points: { show: false },
+                        shadowSize: 0
                     },
-                    points: { show: false },
-                    shadowSize: 0
-                },
-                grid: {
-                    color: 'rgba(0, 0, 0, 0.6)',
-                    borderWidth: 1,
-                    borderColor: 'transparent',
-                    hoverable: true,
-                    clickable: true
-                },
-                tooltip: true,
+                    grid: {
+                        color: 'rgba(0, 0, 0, 0.6)',
+                        borderWidth: 1,
+                        borderColor: 'transparent',
+                        hoverable: true,
+                        clickable: true
+                    },
+                    tooltip: true,
                     tooltipOpts: {
-                    content: "%y %s in %x",
-                    shifts: {
-                        x: -60,
-                        y: 25
+                        content: "%y %s in %x",
+                        shifts: {
+                            x: -60,
+                            y: 25
+                        },
+                        defaultTheme: false
                     },
-                    defaultTheme: false
-                },
-                legend: {
-                    show: false
-                },
-                xaxis: {
-                    mode: "time",
-                    tickDecimals: 0,
-                    tickFormatter: function (val, axis) {
-                        var d = new Date(val);
-                        return (d.getUTCMonth() + 1) + "/" + d.getUTCFullYear().toString().substr(2,2);
-                    }
-                },
-                yaxis: {
-                    min: 0,
-                    tickFormatter: function (val, axis) {
-                        if (val > 1000) {
-                            val = (val / 1000) + ' K';
+                    legend: {
+                        show: false
+                    },
+                    xaxis: {
+                        mode: "time",
+                        tickDecimals: 0,
+                        tickFormatter: function (val, axis) {
+                            var d = new Date(val);
+                            return (d.getUTCMonth() + 1) + "/" + d.getUTCFullYear().toString().substr(2, 2);
                         }
-                        return val;
+                    },
+                    yaxis: {
+                        min: 0,
+                        tickFormatter: function (val, axis) {
+                            if (val > 1000) {
+                                val = (val / 1000) + ' K';
+                            }
+                            return val;
+                        }
                     }
+                });
+                chart_views.highlight(0, response.chart_views - 1);
+
+
+                var downloads = $('#chart-downloads');
+                downloads.bind("plotclick", function (event, pos, item) {
+                    return updateCharts(event, pos, item);
+                });
+
+                let downloads_array = response.downloads.splice(0, response.downloads.length)
+                let downloads_split = []
+                let dataset_downloads = [{
+                    color: response.dataset_downloads.color,
+                    data: downloads_split,
+                    label: response.dataset_downloads.label
+                }]
+                
+                for (i = 0; i < downloads_array.length; i++) {
+                    downloads_split.push(eval(downloads_array[i]))
                 }
-            });
-            chart_downloads.highlight(0, response.chart_downloads - 1);
-        },
-        error: function (xhr, status, error) {
-            
-        }
-    }) 
+
+                var chart_downloads = $.plot(downloads, dataset_downloads, {
+                    series: {
+                        lines: {
+                            show: true,
+                            fill: false
+                        },
+                        points: { show: false },
+                        shadowSize: 0
+                    },
+                    grid: {
+                        color: 'rgba(0, 0, 0, 0.6)',
+                        borderWidth: 1,
+                        borderColor: 'transparent',
+                        hoverable: true,
+                        clickable: true
+                    },
+                    tooltip: true,
+                    tooltipOpts: {
+                        content: "%y %s in %x",
+                        shifts: {
+                            x: -60,
+                            y: 25
+                        },
+                        defaultTheme: false
+                    },
+                    legend: {
+                        show: false
+                    },
+                    xaxis: {
+                        mode: "time",
+                        tickDecimals: 0,
+                        tickFormatter: function (val, axis) {
+                            var d = new Date(val);
+                            return (d.getUTCMonth() + 1) + "/" + d.getUTCFullYear().toString().substr(2, 2);
+                        }
+                    },
+                    yaxis: {
+                        min: 0,
+                        tickFormatter: function (val, axis) {
+                            if (val > 1000) {
+                                val = (val / 1000) + ' K';
+                            }
+                            return val;
+                        }
+                    }
+                });
+                chart_downloads.highlight(0, response.chart_downloads - 1);
+            },
+            error: function (xhr, status, error) {
+                
+            }
+        })
+    }).resize();
 })
